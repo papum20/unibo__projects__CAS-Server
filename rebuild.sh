0fd93d90dbc28a28cff5f778d7559fd643fa3a17
papum_debian <danieledugo1@gmail.com>
2023-10-27 12:03:27 +0200

    nginx params

---
+++ rebuild.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+./down.sh
+docker-compose \
+    -f docker-compose.dashboard.yml \
+    -f docker-compose.gitlab.yml \
+    -f docker-compose.jenkins.yml \
+    -f docker-compose.logger.yml \
+    -f docker-compose.mattermost.yml \
+    -f docker-compose.sonar.yml \
+    -f docker-compose.taiga.yml \
+    -f docker-compose.yml \
+    build --no-cache
+./up.sh
