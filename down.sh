dd0da91854ed93d7bae86088d71144be251200a5
papum_debian <danieledugo1@gmail.com>
2023-10-27 10:45:11 +0200

    fix

---
+++ down.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# down
+docker-compose \
+    -f docker-compose.dashboard.yml \
+    -f docker-compose.gitlab.yml \
+    -f docker-compose.jenkins.yml \
+    -f docker-compose.logger.yml \
+    -f docker-compose.mattermost.yml \
+    -f docker-compose.sonar.yml \
+    -f docker-compose.taiga.yml \
+    -f docker-compose.yml \
+    down
